0684a53474163168b1ac4760b9987182984b5596
kalhn443 <I2evinkub>
2021-08-11 17:00:12 +0700

    mod

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function calculator() {
-  var wallet = document.getElementById('wallet').value;
-  var buyPrice = document.getElementById('buyPrice').value;
-  var sellPrice = document.getElementById('sellPrice').value;
-  var percentPrice = document.getElementById('percentPrice').value;
+  var wallet = document.getElementById('wallet').value.replace(/[\D\s\._\-]+/g, "");
+  var buyPrice = document.getElementById('buyPrice').value.replace(/[\D\s\._\-]+/g, "");
+  var sellPrice = document.getElementById('sellPrice').value.replace(/[\D\s\._\-]+/g, "");
+  var percentPrice = document.getElementById('percentPrice').value.replace(/[\D\s\._\-]+/g, "");
   var unit = parseFloat(wallet) / parseFloat(buyPrice) ;
 
   var wallet1 = (unit * parseFloat(sellPrice)) - ((unit * parseFloat(sellPrice))*0.25/100) ;
@@ -36,3 +36,65 @@ function calculator() {
   }
 
 }
+
+
+
+(function($, undefined) {
+
+	"use strict";
+
+	// When ready.
+	$(function() {
+		
+		var $form = $( "#form" );
+		var $input = $form.find( "input" );
+
+		$input.on( "keyup", function( event ) {
+			
+			
+			// When user select text in the document, also abort.
+			var selection = window.getSelection().toString();
+			if ( selection !== '' ) {
+				return;
+			}
+			
+			// When the arrow keys are pressed, abort.
+			if ( $.inArray( event.keyCode, [38,40,37,39] ) !== -1 ) {
+				return;
+			}
+			
+			
+			var $this = $( this );
+			
+			// Get the value.
+			var input = $this.val();
+			
+			var input = input.replace(/[\D\s\._\-]+/g, "");
+					input = input ? parseInt( input, 10 ) : 0;
+
+					$this.val( function() {
+						return ( input === 0 ) ? "" : input.toLocaleString( "en-US" );
+					} );
+		} );
+		
+		/**
+		 * ==================================
+		 * When Form Submitted
+		 * ==================================
+		 */
+		$form.on( "submit", function( event ) {
+			
+			var $this = $( this );
+			var arr = $this.serializeArray();
+		
+			for (var i = 0; i < arr.length; i++) {
+					arr[i].value = arr[i].value.replace(/[($)\s\._\-]+/g, ''); // Sanitize the values.
+			};
+			
+			console.log( arr );
+			
+			event.preventDefault();
+		});
+		
+	});
+})(jQuery);
