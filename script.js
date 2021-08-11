7ba8eae9da7b5aa749970531a3d9a29cc5ffc049
kalhn443 <I2evinkub>
2021-08-11 16:36:22 +0700

    add

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function calculator() {
+  var wallet = document.getElementById('wallet').value;
+  var buyPrice = document.getElementById('buyPrice').value;
+  var sellPrice = document.getElementById('sellPrice').value;
+  var percentPrice = document.getElementById('percentPrice').value;
+  var unit = parseFloat(wallet) / parseFloat(buyPrice) ;
+
+  var wallet1 = (unit * parseFloat(sellPrice)) - ((unit * parseFloat(sellPrice))*0.25/100) ;
+  var profit1 = wallet1 - parseFloat(wallet);
+  var percent = (parseFloat(sellPrice) - parseFloat(buyPrice))/parseFloat(buyPrice)*100;
+
+
+  var price = parseFloat(buyPrice) + ((parseFloat(buyPrice)*parseFloat(percentPrice))/100);
+  var wallet2 = (unit * parseFloat(price)) - ((unit * parseFloat(price))*0.25/100)         
+  var profit2 = wallet2 - parseFloat(wallet);;
+
+
+ 
+
+  if (!isNaN(wallet1)) {
+      document.getElementById('percent').value = `${new Intl.NumberFormat('en-US',{ maximumSignificantDigits: 3 }).format(percent)} %`;
+      document.getElementById('profit1').value = `${new Intl.NumberFormat('en-US',{ maximumSignificantDigits: 3 }).format(wallet1)} : ${new Intl.NumberFormat('en-US',{ maximumSignificantDigits: 3 }).format(profit1)} `;
+  }else{
+      document.getElementById('percent').value = `%`;
+      document.getElementById('profit1').value = `Profit`;
+
+  }
+  
+  if (!isNaN(wallet2)) {
+      document.getElementById('price').value = `${new Intl.NumberFormat('en-US').format(price)}`;
+      document.getElementById('profit2').value = `${new Intl.NumberFormat('en-US',{ maximumSignificantDigits: 3 }).format(wallet2)} : ${new Intl.NumberFormat('en-US',{ maximumSignificantDigits: 3 }).format(profit2)} `;
+  }else{
+      document.getElementById('price').value = `Sell price`;
+      document.getElementById('profit2').value = `Profit`;
+  }
+
+}
